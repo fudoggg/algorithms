be83c1901470cce73dde87900dc72891cf5fcadd
Kirill <o.kirill-02@mail.ru>
2025-11-26 14:09:45 +0500

    2727. Is Object Empty

---
+++ 2727. Is Object Empty.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/is-object-empty/description/
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0
+};
